4262b52e3fef533ee69701bea7569f19f257c030
Tony <59328777+Technut10@users.noreply.github.com>
2025-04-30 19:28:35 -0600

    Initial commit

---
+++ app/src/main/java/edu/msudenver/cs3013/movielist/Movie.kt
@@ -0,0 +1,7 @@
+package edu.msudenver.cs3013.movielist
+
+open class Movie(val title:String, val yearOfRelease: String, val movieGenre:String, val rating: Double) {
+    override fun toString(): String {
+        return "Title: $title, Year: $yearOfRelease, Movie: $movieGenre, Rating: $rating"
+    }
+}
